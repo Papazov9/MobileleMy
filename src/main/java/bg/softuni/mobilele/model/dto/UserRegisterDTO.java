19e408c6ef6f4ca1a52725706f40407b581bfb36
Благовест Папазов <blagovestpapazov987@gmail.com>
2022-06-04 01:11:30 +0300

    login, logout, register logic implemented

--- src/main/java/bg/softuni/mobilele/model/dto/UserRegisterDTO.java
+++ src/main/java/bg/softuni/mobilele/model/dto/UserRegisterDTO.java
@@ -5,46 +5,53 @@ public class UserRegisterDTO {
     private String firstName;
     private String lastName;
     private String email;
+
     private String password;
+
     private String confirmPassword;
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
+    public UserRegisterDTO setFirstName(String firstName) {
         this.firstName = firstName;
+        return this;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
+    public UserRegisterDTO setLastName(String lastName) {
         this.lastName = lastName;
+        return this;
     }
 
-    public String getUsername() {
+    public String getEmail() {
         return email;
     }
 
-    public void setUsername(String username) {
-        this.email = username;
+    public UserRegisterDTO setEmail(String email) {
+        this.email = email;
+        return this;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
+    public UserRegisterDTO setPassword(String password) {
         this.password = password;
+        return this;
     }
 
     public String getConfirmPassword() {
         return confirmPassword;
     }
 
-    public void setConfirmPassword(String confirmPassword) {
+    public UserRegisterDTO setConfirmPassword(String confirmPassword) {
         this.confirmPassword = confirmPassword;
+        return this;
     }
 }
--- src/main/java/bg/softuni/mobilele/service/UserService.java
+++ src/main/java/bg/softuni/mobilele/service/UserService.java
@@ -1,6 +1,7 @@
 package bg.softuni.mobilele.service;
 
 import bg.softuni.mobilele.model.dto.UserLoginDTO;
+import bg.softuni.mobilele.model.dto.UserRegisterDTO;
 import bg.softuni.mobilele.model.entities.User;
 import bg.softuni.mobilele.repository.UserRepository;
 import bg.softuni.mobilele.user.CurrentUser;
@@ -24,6 +25,20 @@ public class UserService {
         this.userRepository = userRepository;
         this.currentUser = currentUser;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    public void registerAndLogin(UserRegisterDTO userRegisterDTO) {
+        User newUser =
+                new User()
+                        .setActive(true)
+                        .setEmail(userRegisterDTO.getEmail())
+                        .setFirstName(userRegisterDTO.getFirstName())
+                        .setLastName(userRegisterDTO.getLastName())
+                        .setPassword(passwordEncoder.encode(userRegisterDTO.getPassword()));
+
+        newUser = userRepository.save(newUser);
+
+        login(newUser);
     }
 
     public boolean login(UserLoginDTO loginDTO){
